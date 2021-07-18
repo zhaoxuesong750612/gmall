24bdfc2286b73df56f01a8b98614212cc15b4977
zhaoxuesong <zhaoxuesong750612@gmail.com>
2021-07-18 21:25:56 +0800

    Day02 - 微服务架构脚手架

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gmall
+
+gmall-user-service: 8070
+
+gmall-user-web: 8080
--- gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -3,17 +3,17 @@ package com.atguigu.gmall.user.controller;
 import com.atguigu.gmall.service.UserService;
 import com.atguigu.gmall.bean.UmsMember;
 import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.alibaba.dubbo.config.annotation.Reference;
 
 import java.util.List;
 
 @Controller
 public class UserController {
 
-    @Autowired
+    @Reference
     UserService userService;
 
     @RequestMapping("index")
