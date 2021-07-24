c35b46dc18532c0b1cf7e4c3fdf55a7e47c20050
zhaoxuesong <zhaoxuesong750612@gmail.com>
2021-07-24 12:36:26 +0800

    Day03 - 谷粒商品·pms·分类和平台属性

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 gmall-user-service: 8070
 
 gmall-user-web: 8080
+
+gmall-manage-service: 8071
+
+gmall-manage-web: 8081
---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/AttrServiceImpl.java
@@ -0,0 +1,69 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
+import com.atguigu.gmall.manage.mapper.PmsBaseAttrInfoMapper;
+import com.atguigu.gmall.manage.mapper.PmsBaseAttrValueMapper;
+import com.atguigu.gmall.service.AttrService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class AttrServiceImpl implements AttrService {
+
+    @Autowired
+    PmsBaseAttrInfoMapper pmsBaseAttrInfoMapper;
+    @Autowired
+    PmsBaseAttrValueMapper pmsBaseAttrValueMapper;
+
+    @Override
+    public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id) {
+        PmsBaseAttrInfo pmsBaseAttrInfo = new PmsBaseAttrInfo();
+        pmsBaseAttrInfo.setCatalog3Id(catalog3Id);
+        List<PmsBaseAttrInfo> pmsBaseAttrInfos = pmsBaseAttrInfoMapper.select(pmsBaseAttrInfo);
+        return pmsBaseAttrInfos;
+    }
+
+    @Override
+    public String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo) {
+        String id = pmsBaseAttrInfo.getId();
+        if (StringUtils.isBlank(id)) {
+            pmsBaseAttrInfoMapper.insertSelective(pmsBaseAttrInfo);
+
+            List<PmsBaseAttrValue> attrValueList = pmsBaseAttrInfo.getAttrValueList();
+            for (PmsBaseAttrValue pmsBaseAttrValue : attrValueList) {
+                pmsBaseAttrValue.setAttrId(pmsBaseAttrInfo.getId());
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            }
+        } else {
+            // modify attribute
+            Example e = new Example(PmsBaseAttrInfo.class);
+            e.createCriteria().andEqualTo("id", pmsBaseAttrInfo.getId());
+            pmsBaseAttrInfoMapper.updateByExampleSelective(pmsBaseAttrInfo, e);
+
+            // delete all attribute values by attribute
+            PmsBaseAttrValue pmsBaseAttrValueDel = new PmsBaseAttrValue();
+            pmsBaseAttrValueDel.setAttrId(pmsBaseAttrInfo.getId());
+            pmsBaseAttrValueMapper.delete(pmsBaseAttrValueDel);
+
+            // insert all attribute values
+            List<PmsBaseAttrValue> attrValueList = pmsBaseAttrInfo.getAttrValueList();
+            for (PmsBaseAttrValue pmsBaseAttrValue : attrValueList) {
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            }
+        }
+        return "success";
+    }
+
+    @Override
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId) {
+        PmsBaseAttrValue pmsBaseAttrValue = new PmsBaseAttrValue();
+        pmsBaseAttrValue.setAttrId(attrId);
+        List<PmsBaseAttrValue> pmsBaseAttrValues = pmsBaseAttrValueMapper.select(pmsBaseAttrValue);
+        return pmsBaseAttrValues;
+    }
+}
